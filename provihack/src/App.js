c4abe01560273953aea62c1ad326621923a77376
Natália Barbosa <nob.ufba@gmail.com>
2021-11-27 12:29:55 -0300

    Configurações para renderizar o mapa, ainda não é possivel atualizar localização

--- provihack/src/App.js
+++ provihack/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoibmF0YWxpYS1iYXJib3NhIiwiYSI6ImNrd2h2cjRxYzEya2kycXF3MXllOWc3cnkifQ.VFt7h46Rk7bGRVZfg5VY_w';
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
   return (
     <div className="App">
-      <h1>Homologação</h1>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
